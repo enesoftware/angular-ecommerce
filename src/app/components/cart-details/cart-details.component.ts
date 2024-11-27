c1b61e6b793d27ab25699723577dfda932946f75
enesoftware <144807405+enesoftware@users.noreply.github.com>
2024-11-27 23:56:44 +0300

    shopping cart details increment item quantity

--- src/app/components/cart-details/cart-details.component.ts
+++ src/app/components/cart-details/cart-details.component.ts
@@ -38,4 +38,9 @@ export class CartDetailsComponent implements OnInit {
     // compute cart total price and quantity
     this.cartService.computeCartTotals();
   }
+
+  incrementQuantity(theCartItem: CartItem) {
+    this.cartService.addToCart(theCartItem);
+  }
+
 }
